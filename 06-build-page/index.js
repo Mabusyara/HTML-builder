f25570e5ca918734b595d16be7adf64f2cfbf314
Aleksey Sobolev <iammabus@gmail.com>
2024-01-23 01:09:51 +0300

    feat: complete 06 task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,34 +1,45 @@
-/* eslint-disable prettier/prettier */
 const fsPromises = require('node:fs/promises');
-const fs = require('node:fs');
 const path = require('node:path');
 
 const projectPath = path.join(__dirname, 'project-dist');
 
-function createNewFolder(where, name) {
-  fsPromises.mkdir(path.join(where, name), { recursive: true });
+async function createNewFolder(where, name) {
+  await fsPromises.mkdir(path.join(where, name), { recursive: true });
 }
 
 async function replaceTemplateTags() {
-  await fsPromises.copyFile(path.join(__dirname, 'template.html'), path.join(projectPath, 'index.html'));
-  let newHtml = await fsPromises.readFile(path.join(projectPath, 'index.html'), 'utf-8');
+  await fsPromises.copyFile(
+    path.join(__dirname, 'template.html'),
+    path.join(projectPath, 'index.html'),
+  );
+  let newHtml = await fsPromises.readFile(
+    path.join(projectPath, 'index.html'),
+    'utf-8',
+  );
   const files = await fsPromises.readdir(path.join(__dirname, 'components'));
   for (const file of files) {
-    await fsPromises.readFile(path.join(__dirname, 'components', file), 'utf-8').then((data) => {
-      newHtml = newHtml.toString().replaceAll(`{{${path.basename(file, '.html')}}}`, data.toString());
-    });
+    await fsPromises
+      .readFile(path.join(__dirname, 'components', file), 'utf-8')
+      .then((data) => {
+        newHtml = newHtml
+          .toString()
+          .replaceAll(`{{${path.basename(file, '.html')}}}`, data.toString());
+      });
   }
   await fsPromises.writeFile(path.join(projectPath, 'index.html'), newHtml);
 }
 
-async function mergeStyles () {
+async function mergeStyles() {
   await fsPromises.writeFile(path.join(projectPath, 'style.css'), '');
   const cssArray = [];
   const files = await fsPromises.readdir(path.join(__dirname, 'styles'));
   for (const file of files) {
     const stats = await fsPromises.stat(path.join(__dirname, 'styles', file));
     if (stats.isFile() && path.extname(file) === '.css') {
-      const temp = await fsPromises.readFile(path.join(__dirname, 'styles', file), 'utf-8');
+      const temp = await fsPromises.readFile(
+        path.join(__dirname, 'styles', file),
+        'utf-8',
+      );
       cssArray.push(temp.toString());
     }
   }
@@ -37,10 +48,20 @@ async function mergeStyles () {
   }
 }
 
+async function copyFiles(from, to) {
+  const files = await fsPromises.readdir(from);
+  for (const file of files) {
+    const stats = await fsPromises.stat(path.join(from, file));
+    if (stats.isDirectory()) {
+      await createNewFolder(to, file);
+      await copyFiles(path.join(from, file), path.join(to, file));
+    } else
+      await fsPromises.copyFile(path.join(from, file), path.join(to, file));
+  }
+}
+
 createNewFolder(__dirname, 'project-dist');
-// createNewFolder(projectPath, 'assets');
-// createNewFolder(path.join(projectPath, 'assets'), 'fonts');
-// createNewFolder(path.join(projectPath, 'assets'), 'img');
-// createNewFolder(path.join(projectPath, 'assets'), 'svg');
+createNewFolder(projectPath, 'assets');
 replaceTemplateTags();
 mergeStyles();
+copyFiles(path.join(__dirname, 'assets'), path.join(projectPath, 'assets'));
